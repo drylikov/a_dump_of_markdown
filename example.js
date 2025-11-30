ef628804638b9e60d0fdcdebada2aa7bf834ca8a
drylikov <denis.rylikov@protonmail.com>
2025-11-30 16:41:38 +0300

    a_dump_of_markdown

---
+++ example.js
@@ -0,0 +1,14 @@
+
+var fs = require("fs");
+var m = require("micromark");
+var marky = require("marky");
+
+var doc = fs.readFileSync("markdown.md");
+
+marky.mark("micromark");
+var x = m(doc);
+var entry = marky.stop("micromark");
+
+if (typeof document !== "undefined") document.body.innerHTML = x;
+
+console.log(entry.duration);
